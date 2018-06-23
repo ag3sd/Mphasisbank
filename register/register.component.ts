426c7c36fc4f2a2d2dc570ac664702ea8e9ad0d6
ag3sd <30552460+ag3sd@users.noreply.github.com>
2018-06-23 09:19:59 +0530

    Add files via upload

---
+++ register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  name:string;
+  password:string;
+  confmpassword:string;
+  email:string;
+
+  constructor() {
+    
+  }
+  test(refname:object) {
+    console.log(refname);
+  }
+  ngOnInit() {
+
+  }
+  reg
+}
+
+
