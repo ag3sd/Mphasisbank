d5e090d587776ba9195cb36485106f7cca60882d
Jeevanthimovva13 <39362920+Jeevanthimovva13@users.noreply.github.com>
2018-06-25 20:02:27 +0530

    Add files via upload

---
+++ personaldetails/personaldetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'personaldetails',
+  templateUrl: './personaldetails.component.html',
+  styleUrls: ['./personaldetails.component.css']
+})
+export class PersonaldetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
