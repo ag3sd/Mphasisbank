d1cde5d82dab3b5c1b84e9d969f10cc0de7b9dfd
tejasri169 <tejasri169@gmail.com>
2018-06-26 14:44:11 +0530

    Add files via upload

---
+++ cus-details/cus-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cus-details',
+  templateUrl: './cus-details.component.html',
+  styleUrls: ['./cus-details.component.css']
+})
+export class CusDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
